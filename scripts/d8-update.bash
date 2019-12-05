49ce27a7e5f2df725759628871d5b808868f29fe
Peter Carrero <peter.carrero@gmail.com>
2019-12-05 14:54:56 -0600

    scripts: Add first version of d8-update script

--- scripts/d8-update.bash
+++ scripts/d8-update.bash
@@ -2,10 +2,10 @@
 
 function usage() {
   echo "This script updates a Drupal 8 module via composer."
-  echo ""
-  echo "Usage:"
-  echo "$0 <module name|core>"
   echo "If 'core' is used as a parameter, the script will attempt to update Drupal core."
+  echo ""
+  echo "Usage: $0 <module name|core>"
+  echo ""
   exit 1
 }
 
@@ -13,6 +13,13 @@ function error() {
    echo "ERROR: $@"
    echo ""
    usage
+}
+
+function logText() {
+  if [ $@ -gt 0 ]; then
+    day=$(date "+%Y-%m-%d-%H-%M-%S")
+    echo "$day - $@"
+  fi
 }
 
 if [ $# -ne 1 ]; then 
@@ -23,3 +30,12 @@ if [ $(git rev-parse --show-toplevel &> /dev/null; echo $?) -ne 0 ]; then
   error "this is not a git repository. Switch to inside a git repo and retry."
 fi
 
+MODULE="$1"
+MOD_MSG="$(if [ "${MODULE}" == "core" ]; then echo -ne "Drupal "; fi)${MODULE}"
+logText "Starting update process"
+logText "Looking to update ${MODULE}"
+composer update drupal/${MODULE} --with-dependencies && \
+  git add -u && \
+  git ci -m "secreview: Update ${MOD_MSG} to $(composer show drupal/${MODULE} | sed -ne 's/^version.* //gp')" && \
+  git push
+logText "Update process done"
